162c906e48a5d533501e94e7114e7383a6e8f5f6
GaPPy the Capy <david.davko@gmail.com>
2025-05-23 22:26:03 +0200

    Close socket on unload

--- WebView/script.js
+++ WebView/script.js
@@ -1,22 +1,32 @@
-const socket = new WebSocket("ws://localhost:5000/ws");
+let socket;
+const webURL = "ws://localhost:5000/ws";
 
 const lerpDuration = 300;
 const lerp = (a, b, t) => a + (b - a) * t;
 
-let prevEntities = {};
-let entities = {
-  animal: {},
-  food: {},
-};
-let lastUpdate = performance.now();
+let prevEntities;
+let entities;
+let lastUpdate;
 
-let recentlySpawned = {
-  animal: {},
-  food: {},
-};
-let recentlyDespawned = {
-  animal: {},
-  food: {},
+let recentlySpawned;
+let recentlyDespawned;
+
+const resetState = () => {
+  prevEntities = {};
+  entities = {
+    animal: {},
+    food: {},
+  };
+  lastUpdate = performance.now();
+
+  recentlySpawned = {
+    animal: {},
+    food: {},
+  };
+  recentlyDespawned = {
+    animal: {},
+    food: {},
+  };
 };
 
 const applyDiff = (cache, diffs, type) => {
@@ -40,62 +50,69 @@ const applyDiff = (cache, diffs, type) => {
   });
 };
 
-socket.onmessage = (event) => {
-  const { animals, foods, activeClients, timeFromStart, reignitions } =
-    JSON.parse(event.data);
-  lastUpdate = performance.now();
+let createConnection = () => {
+  resetState();
+  socket = new WebSocket(webURL);
 
-  prevEntities = JSON.parse(JSON.stringify(entities));
+  socket.onmessage = (event) => {
+    const { animals, foods, activeClients, timeFromStart, reignitions } =
+      JSON.parse(event.data);
+    lastUpdate = performance.now();
 
-  applyDiff(entities, animals, "animal");
-  applyDiff(entities, foods, "food");
+    prevEntities = JSON.parse(JSON.stringify(entities));
 
-  let animalCounts = {
-    HERBIVORE: 0,
-    CARNIVORE: 0,
-    OMNIVORE: 0,
-  };
+    applyDiff(entities, animals, "animal");
+    applyDiff(entities, foods, "food");
 
-  for (const animal of Object.values(entities["animal"])) {
-    animalCounts[animal.foodType] += 1;
-  }
+    let animalCounts = {
+      HERBIVORE: 0,
+      CARNIVORE: 0,
+      OMNIVORE: 0,
+    };
 
-  const animalCount = Object.keys(entities["animal"]).length;
-  const foodCount = Object.keys(entities["food"]).length;
-
-  const total = animalCount + foodCount;
-  const animalPercent = fractionToPercentage(animalCount / total);
-  const foodPercent = fractionToPercentage(foodCount / total);
-
-  let animalCountsPercentage = {};
-
-  if (animalCount === 0) {
-    animalCountsPercentage = animalCounts;
-  } else {
-    for (let type in animalCounts) {
-      animalCountsPercentage[type] = fractionToPercentage(
-        animalCounts[type] / animalCount,
-        1,
-      );
+    for (const animal of Object.values(entities["animal"])) {
+      animalCounts[animal.foodType] += 1;
     }
-  }
 
-  viewerCountHeader.innerText =
-    activeClients === 1
-      ? `${activeClients} viewer`
-      : `${activeClients} viewers`;
+    const animalCount = Object.keys(entities["animal"]).length;
+    const foodCount = Object.keys(entities["food"]).length;
 
-  entityCategoryDisplays[0].innerText = `Animals: ${animalCount}\n${animalPercent}% `;
-  entityCategoryDisplays[1].innerText = `Food: ${foodCount}\n${foodPercent}%`;
+    const total = animalCount + foodCount;
+    const animalPercent = fractionToPercentage(animalCount / total);
+    const foodPercent = fractionToPercentage(foodCount / total);
 
-  animalFoodTypeDisplays[0].innerText = `Herbivores: ${animalCounts["HERBIVORE"]}\n${animalCountsPercentage["HERBIVORE"]}%`;
-  animalFoodTypeDisplays[1].innerText = `Carnivores: ${animalCounts["CARNIVORE"]}\n${animalCountsPercentage["CARNIVORE"]}%`;
-  animalFoodTypeDisplays[2].innerText = `Omnivores: ${animalCounts["OMNIVORE"]}\n${animalCountsPercentage["OMNIVORE"]}%`;
+    let animalCountsPercentage = {};
 
-  elapsedTimeHeader.innerText = `Elapsed time: ${getTimeString(timeFromStart)}`;
-  reignitionCounter.innerText = `Reignition count: ${reignitions}`;
+    if (animalCount === 0) {
+      animalCountsPercentage = animalCounts;
+    } else {
+      for (let type in animalCounts) {
+        animalCountsPercentage[type] = fractionToPercentage(
+          animalCounts[type] / animalCount,
+          1
+        );
+      }
+    }
 
-  reigniteLifeButton.disabled = animalCount > 0;
+    viewerCountHeader.innerText =
+      activeClients === 1
+        ? `${activeClients} viewer`
+        : `${activeClients} viewers`;
+
+    entityCategoryDisplays[0].innerText = `Animals: ${animalCount}\n${animalPercent}% `;
+    entityCategoryDisplays[1].innerText = `Food: ${foodCount}\n${foodPercent}%`;
+
+    animalFoodTypeDisplays[0].innerText = `Herbivores: ${animalCounts["HERBIVORE"]}\n${animalCountsPercentage["HERBIVORE"]}%`;
+    animalFoodTypeDisplays[1].innerText = `Carnivores: ${animalCounts["CARNIVORE"]}\n${animalCountsPercentage["CARNIVORE"]}%`;
+    animalFoodTypeDisplays[2].innerText = `Omnivores: ${animalCounts["OMNIVORE"]}\n${animalCountsPercentage["OMNIVORE"]}%`;
+
+    elapsedTimeHeader.innerText = `Elapsed time: ${getTimeString(
+      timeFromStart
+    )}`;
+    reignitionCounter.innerText = `Reignition count: ${reignitions}`;
+
+    reigniteLifeButton.disabled = animalCount > 0;
+  };
 };
 
 const renderEntity = (position, diameter, color, outline) => {
@@ -201,4 +218,14 @@ document.addEventListener("DOMContentLoaded", () => {
       el.style.transform = `perspective(${PERSPECTIVE}px) rotateX(0deg) rotateY(0deg)`;
     });
   });
+});
+
+createConnection();
+
+document.addEventListener("visibilitychange", () => {
+  if (document.hidden) {
+    socket.close();
+  } else {
+    createConnection();
+  }
 });
