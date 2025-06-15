54a7fe44ac336ad0371b0dda4cd7b6c7d83d449e
LordDeatHunter <desuhansa@gmail.com>
2025-06-15 17:36:42 +0200

    Add initial migrations

---
+++ LifeSim/Migrations/20250615021704_InitialCreate.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LifeSim.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    X = table.Column<float>(type: "REAL", nullable: false),
+                    Y = table.Column<float>(type: "REAL", nullable: false),
+                    ColorHex = table.Column<string>(type: "TEXT", maxLength: 7, nullable: false),
+                    Size = table.Column<float>(type: "REAL", nullable: false),
+                    PredationInclination = table.Column<float>(type: "REAL", nullable: false),
+                    Saturation = table.Column<float>(type: "REAL", nullable: false),
+                    ReproductionCooldown = table.Column<float>(type: "REAL", nullable: false),
+                    Speed = table.Column<float>(type: "REAL", nullable: false),
+                    Age = table.Column<float>(type: "REAL", nullable: false),
+                    Lifespan = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Foods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    X = table.Column<float>(type: "REAL", nullable: false),
+                    Y = table.Column<float>(type: "REAL", nullable: false),
+                    ColorHex = table.Column<string>(type: "TEXT", maxLength: 7, nullable: false),
+                    Size = table.Column<float>(type: "REAL", nullable: false),
+                    Age = table.Column<float>(type: "REAL", nullable: false),
+                    Lifespan = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Foods", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ClientId = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    Balance = table.Column<ulong>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ClientId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ClientId = table.Column<string>(type: "TEXT", nullable: false),
+                    Amount = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    BetType = table.Column<string>(type: "TEXT", nullable: false),
+                    InitialCount = table.Column<int>(type: "INTEGER", nullable: false),
+                    ExpiresAt = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    UserClientId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bets_Users_UserClientId",
+                        column: x => x.UserClientId,
+                        principalTable: "Users",
+                        principalColumn: "ClientId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bets_UserClientId",
+                table: "Bets",
+                column: "UserClientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+
+            migrationBuilder.DropTable(
+                name: "Bets");
+
+            migrationBuilder.DropTable(
+                name: "Foods");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
